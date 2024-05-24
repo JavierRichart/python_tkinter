b7d5d28285845a218d5469294118d63000050684
JavierRichart <lopezrichart@gmail.com>
2024-05-24 07:57:34 +0200

    Pruebas grid

--- metodo_grid.py
+++ metodo_grid.py
@@ -3,50 +3,44 @@ import tkinter as tk
 # Ventana principal
 
 ventana = tk.Tk()
-ventana.geometry("600x500+500+100")
+# ventana.geometry("600x500+500+100")
 
 # Aplicación
 
 uno = tk.Button(ventana,
                 text="1",
                 font="consolas 30",
-                bg="lightgreen",
-                width=3)
-uno.grid(row=0, column=0, padx=10, pady=10)
+                bg="lightgreen")
+uno.grid(row=0, column=0, sticky="nsew")
 
 dos = tk.Button(ventana,
                 text="2",
-                font="consolas 30",
-                bg="lightgreen",
-                width=3)
-dos.grid(row=0, column=1, padx=10, pady=10)
+                font="consolas 50",
+                bg="lightgreen")
+dos.grid(row=0, column=1, sticky="nsew")
 
 tres = tk.Button(ventana,
                  text="3",
-                 font="consolas 30",
-                 bg="lightgreen",
-                 width=3)
-tres.grid(row=0, column=2, padx=10, pady=10)
+                 font="consolas 20",
+                 bg="lightgreen",)
+tres.grid(row=0, column=2, sticky="nsew")
 
 cuatro = tk.Button(ventana,
-                   text="4",
+                   text="  4  ",
                    font="consolas 30",
-                   bg="lightgreen",
-                   width=3)
-cuatro.grid(row=1, column=0, padx=10, pady=10)
+                   bg="lightgreen",)
+cuatro.grid(row=1, column=0, sticky="nsew")
 
 cinco = tk.Button(ventana,
                   text="5",
-                  font="consolas 30",
-                  bg="lightgreen",
-                  width=3)
-cinco.grid(row=2, column=0, columnspan=2, padx=10, pady=10)
+                  font="consolas 10",
+                  bg="lightgreen",)
+cinco.grid(row=1, column=1, sticky="nsew")
 
 seis = tk.Button(ventana,
                  text="6",
-                 font="consolas 30",
-                 bg="lightgreen",
-                 width=3)
-seis.grid(row=3, rowspan=2, column=0, padx=10, pady=10)
+                 font="consolas 70",
+                 bg="lightgreen",)
+seis.grid(row=1, column=2, sticky="nsew")
 
 ventana.mainloop()
